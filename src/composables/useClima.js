4f77b92d49fd65608d608d09b936e606f4649ba6
unknown <jorgedanielpacosillo@gmail.com>
2026-07-22 00:46:40 -0400

    proyecto hecho con vuejs3, composables, consumo de API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -4,6 +4,9 @@ export default function useClima() {
     const key = import.meta.env.VITE_API_KEY;
 
     // Obtener la lat , lng
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=87383f8408377113dcf38dde52c810e2`;
+    console.log(url);
+    
 
     //Obtener el clima
   };
